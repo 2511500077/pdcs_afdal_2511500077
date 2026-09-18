9c9a2c14b707306d51f5b1e83b72960899ad7bcd
AFDAL <2511500077@mahasiswa.atmaluhur.ac.id>
2026-09-18 17:57:33 +0700

    commit kedua

---
+++ mahasiswa.php
@@ -0,0 +1,13 @@
+{
+    "status": "success",
+    "message": "Data mahasiswa berhasil diambil",
+    "data": {
+        "id": 1,
+        "nim": "2511500077",
+        "nama": "afdal",
+        "email": "2511500077@mahasiswa.atmaluhur.ac.id",
+        "jurusan": "Teknik Informatika",
+        "smester": 5,
+        "ipk": 3.85
+    }
+}
